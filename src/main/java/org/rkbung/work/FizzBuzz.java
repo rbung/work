f097c2a0836747c323e76042745e77f40083a17b
rkbung <rkbung@gmail.com>
2012-08-11 19:27:05 +0200

    Mise en place projet

---
+++ src/main/java/org/rkbung/work/FizzBuzz.java
@@ -0,0 +1,13 @@
+package org.rkbung.work;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+
+    public List<String> getList(){
+        List<String> result = new ArrayList<>();
+        result.add("test");
+        return result;
+    }
+}
